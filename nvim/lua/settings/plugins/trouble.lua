43c96110c89d9f00c00e661d8855235f047c02a2
Stuart Dum <stuart.dum@gmail.com>
2024-12-13 14:15:00 -0500

    Update trouble mappings

--- nvim/lua/settings/plugins/trouble.lua
+++ nvim/lua/settings/plugins/trouble.lua
@@ -1,9 +1,36 @@
 return {
 	"folke/trouble.nvim",
 	opts = { icons = false },
-	config = function()
-		vim.keymap.set("n", "<leader>t", function()
-			vim.cmd.TroubleToggle()
-		end)
-	end,
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
